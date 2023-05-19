58494e013475d8e9179f627bcd7ff978f9a6d92b
Jorge Pineda <jpineda@pasteur.com.co>
2023-05-18 22:21:01 -0500

    calculate operation

---
+++ Calculadora.Mobile/Class/Calculator.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculadora.Mobile.Class
+{
+    public static class Calculator
+    {
+        public static double Calculate(double date1, double date2, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "*":
+                    result = date1 * date2;
+                    break;
+                case "/":
+                    result = date1 / date2;
+                    break;
+                case "-":
+                    result = date1 - date2;
+                    break;
+                case "+":
+                    result = date1 + date2;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        } 
+    }
+}
--- Calculadora.Mobile/MainPage.xaml.cs
+++ Calculadora.Mobile/MainPage.xaml.cs
@@ -1,24 +1,87 @@
-﻿namespace Calculadora.Mobile
+﻿using Calculadora.Mobile.Class;
+
+namespace Calculadora.Mobile
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        int state = 1;
+        string operation;
+        double number1, number2;
 
         public MainPage()
         {
             InitializeComponent();
+            OnClear(null,null);
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        void OnClear(object sender, EventArgs e)
         {
-            count++;
+            number1= 0;
+            number2= 0;
+            state= 1;
+            this.resultado.Text= "0";
+        }
 
-            //if (count == 1)
-            //    CounterBtn.Text = $"Clicked {count} time";
-            //else
-            //    CounterBtn.Text = $"Clicked {count} times";
+        void SquareRoot(object sender, EventArgs e)
+        {
+            if (number1 == 0)
+            {
+                return;
+            }
 
-            //SemanticScreenReader.Announce(CounterBtn.Text);
+            number1 = number1 * number1;
+            this.resultado.Text = number1.ToString();
+        }
+        
+        void SelectionNumber(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            string btnAction = button.Text;
+
+            if (this.resultado.Text == "0" || state < 0)
+            {
+                this.resultado.Text = string.Empty;
+                if (state < 0)
+                {
+                    state *= -1;
+                }
+            }
+
+            this.resultado.Text += btnAction;
+
+            double num;
+            if(double.TryParse(this.resultado.Text, out num))
+            {
+                this.resultado.Text = num.ToString("N0");
+                if (state == 1)
+                {
+                    number1 = num;
+                }
+                else
+                {
+                    number2 = num;
+                }
+            }
+        }
+
+        void OperationSelected(object sender, EventArgs e)
+        {
+            state = -2;
+            Button button = sender as Button;
+            string btnAction = button.Text;
+            operation= btnAction;
+        }
+
+        void Calculate(object sender, EventArgs e)
+        {
+            if (state == 2)
+            {
+                var result2 = Calculator.Calculate(number1,number2,operation);
+
+                this.resultado.Text = result2.ToString();
+                number1 = result2;
+                state = -1;
+            }
         }
     }
 }
